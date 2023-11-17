88eeccd41c7ecc02350fa7a0f96be5115827e2af
Tze Tseng Soh <144641936+souffle17@users.noreply.github.com>
2023-11-17 16:17:45 -0500

    optimize

--- src/rsa.cpp
+++ src/rsa.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <numeric>
-
-#include <iostream>
+#include <chrono>
 
 void euclid (long long a, long long b, long long &x, long long &y){
 	if(a == 0) {
@@ -39,6 +38,9 @@ namespace rsa {
             long long n = std::stoll(pk);
             long long e = std::stoll(pe);
 
+            /* make sure n isnt too small */
+            if(n < 2) {return ":x: Not a valid key";}
+
             /* make sure e is prime */
             for(long long i = 2; i < e/2; i++) {
                 if (e % i == 0) {
@@ -46,38 +48,44 @@ namespace rsa {
                 }
             }
 
-            long long p = 0;
-            long long q = 0;
+            auto startTime = std::chrono::system_clock::now();
 
+            long long p = 2;
+            long long q;
 
-            for(long long i = 3; i < n; i+=2) {
-                if (n % i == 0) {
-                    /* store the multiples */
-                    p = i;
-                    q = n / p;
-                    break;
+            long long max = n / p;
+
+            if(n % 2 == 1) {
+                for(long long i = 3; i < max; i+=2) {
+                    if (n % i == 0) {
+                        if(p == 2) {
+                            /* store the multiple */
+                            p = i;
+                            max = n / p;
+                        }
+                        else {
+                            return ":x: Not a valid key";
+                        }
+                    }
                 }
             }
-
-            /* make sure p and q are prime */
-            for(long long i = 2; i < p; i++) {
-                if (p % i == 0) {
-                    return ":x: Not a valid key";
-                }
-            }
-            for(long long i = 2; i < q; i++) {
-                if (q % i == 0) {
-                    return ":x: Not a valid key";
-                }
-            }
+            q = n / p;
+            
 
             long long lcm = (p - 1) * ((q - 1) / std::gcd<int64_t>(p - 1, q - 1));
 
-            if(n % 2 == 0 || n < 2 || e < 2 || e > lcm || lcm % e == 0) {return ":x: Not a valid key";}
+            /* make sure modular multiplicative inverse can be taken */
+            if(e < 2 || e > lcm || lcm % e == 0) {return ":x: Not a valid key";}
 
             long long x, y;
             euclid(e, lcm, x, y);
-            return std::to_string((y + lcm) % lcm);
+
+            long long privateKey = (y + lcm) % lcm;
+
+            auto endTime = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds = endTime - startTime;
+
+            return std::to_string(privateKey) + "\nTook " + std::to_string(elapsed_seconds.count()) + " seconds";
         }
         catch (const std::exception& e) {
             return ":x: Not a valid key";
